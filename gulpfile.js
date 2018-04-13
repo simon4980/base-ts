71598b2a9556710e01601293c06301d8f419d0da
Simon Laug <simonlaug@Simons-MacBook-Air.local>
2018-04-13 16:05:54 -0600

    Updated gulp to allow inlining of js and css on run

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,9 @@ var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var cssnano = require('gulp-cssnano');
-var notify = require('gulp-notify');
 var cache = require('gulp-cache');
 var imagemin = require('gulp-imagemin');
+var inlinesource = require('gulp-inline-source');
 
 var scsslocation = 'src/scss/*.scss';
 var imglocation = 'src/img/**/*';
@@ -29,8 +29,7 @@ function bundle() {
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest("dist/js"))
-        .pipe(connect.reload())
-        .pipe(notify({ message: 'Typescript task complete' }));
+        .pipe(connect.reload());
 }
 
 // Live server reload
@@ -56,9 +55,7 @@ gulp.task('typescript', function(){
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest("dist/js"))
-    .pipe(connect.reload())
-    .pipe(notify({ message: 'Typescript task complete' }));
+    .pipe(gulp.dest("dist/js"));
 });
 
 // Styles
@@ -69,9 +66,7 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('dist/css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cssnano())
-    .pipe(gulp.dest('dist/css'))
-    .pipe(connect.reload())
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(gulp.dest('dist/css'));
 });
 
 // Images
@@ -79,20 +74,29 @@ gulp.task('images', function() {
     return gulp.src(imglocation)
         .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
         .pipe(gulp.dest('dist/img/'))
-        .pipe(connect.reload())
-        .pipe(notify({ message: 'Images task complete' }));
+        .pipe(connect.reload());
+});
+
+// Copy html file from src/html to dist/
+gulp.task("copy-html", ['styles', 'typescript'], function () {
+    return gulp.src('src/html/*.html')
+        .pipe(inlinesource({rootpath: 'dist/'}))
+        .pipe(gulp.dest("dist"))
+        .pipe(connect.reload());
 });
 
 // Watch
 gulp.task('watch', function() {
 
     // Watch .scss files
-    gulp.watch(scsslocation, ['styles']);
+    gulp.watch(scsslocation, ['styles', 'copy-html']);
     // Watch image files
     gulp.watch(imglocation, ['images']);
     // Watch typescript files
-    gulp.watch('src/typescript/**/*.ts', ['typescript']);
-
+    gulp.watch('src/typescript/**/*.ts', ['typescript', 'copy-html']);
+    // Watch html files
+    gulp.watch('src/html/*.html', ['copy-html']);
 });
 
-gulp.task("default", ['styles', 'connect', 'images', 'typescript', 'watch']);
+gulp.task("default", ['styles', 'connect', 'images', 'typescript', 'copy-html', 'watch']);
+
